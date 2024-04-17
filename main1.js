735032aa641f44325e19e75de360fc261de85ae5
Shresth Sharma <76861631+Shresth-Sharma@users.noreply.github.com>
2024-04-17 14:59:16 +0530

    Add files via upload

---
+++ main1.js
@@ -0,0 +1,23 @@
+database = firebase.database();
+var screenWidth = window.innerWidth;
+var screenHeight = window.innerHeight;
+var gameStateRef  = database.ref('data/height');
+gameStateRef.on("value",function(data){
+    totalHeight = data.val();
+    setInterval(repeat, 1000)
+});
+function set(per){
+    if(per >= 0 && per <= 100){
+        document.getElementById('level').style.height = (75/100)*per + 'vh';
+        document.getElementById('level').style.marginTop = (75/100)*(100-per) + 'vh';
+        document.getElementById('leveltext').innerHTML = per + '%';
+    }
+}
+function repeat(){
+    var gameStateRef  = database.ref('data/b');
+    gameStateRef.on("value",function(data){
+        n2 = data.val();
+        per = (n2/totalHeight)*100;
+        set(per)
+    });
+}
